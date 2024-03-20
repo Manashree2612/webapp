674082f20f3175165276e80f2cb65f7a02f91880
mana9512 <manashreepatel28@gmail.com>
2024-03-20 16:35:31 -0400

    moved yml content to separate file

--- packer/setup.sh
+++ packer/setup.sh
@@ -45,23 +45,7 @@ curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 sudo mkdir -p /var/log/webapp
 
-echo 'logging:
-  receivers:
-    my-app-receiver:
-      type: files
-      include_paths:
-        - /var/log/webapp/csye6225.log
-      record_log_file_path: true
-  processors:
-    my-app-processor:
-      type: parse_json
-      time_key: time
-      time_format: "%Y-%m-%dT%H:%M:%S"
-  service:
-    pipelines:
-      default_pipeline:
-        receivers: [my-app-receiver]
-        processors: [my-app-processor]' | sudo tee /etc/google-cloud-ops-agent/config.yaml > /dev/null
+sudo cp /opt/csye6225/packer/config.yml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
 
 echo "================================================================="
