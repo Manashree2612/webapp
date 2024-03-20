e04d88109fcc5c48b09ee225c657cd66ac3e18b6
mana9512 <manashreepatel28@gmail.com>
2024-03-20 14:51:44 -0400

    changed method name

--- src/routes/healthz.js
+++ src/routes/healthz.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { handleGetRequest, handleRetrictedMethods } = require('../controllers/healthz.controller.js');
+const { handleGetRequest, handleRestrictedMethods } = require('../controllers/healthz.controller.js');
 
 const router = express.Router();
 
 router.get('/', handleGetRequest);
 // Middleware to send an error response for non-GET requests
-router.use((req, res, next) => handleRetrictedMethods(req, res, next));
+router.use((req, res, next) => handleRestrictedMethods(req, res, next));
 
 module.exports = router;
