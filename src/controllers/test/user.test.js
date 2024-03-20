e676c868c8fa6589c000ac49341a2ec326256229
mana9512 <manashreepatel28@gmail.com>
2024-03-20 19:24:18 -0400

    updated integration test

--- src/controllers/test/user.test.js
+++ src/controllers/test/user.test.js
@@ -1,35 +1,36 @@
 const chai = require('chai');
 const supertest = require('supertest');
 const app = require('../../app');
-const { expect } = chai;
+const db = require('../../models/index.js');
 
-const userController = require('../user.controller');
-const verifyToken = require('../auth.controller');
+const { expect } = chai;
 
 const request = supertest(app);
 
-
 describe('Integration Tests for user api', () => {
 
-    it('Test 1: Create an account and validate it exists', async () => {
+    it('Create account to validate it exists', async () => {
+      
         await request.post('/v1/user').send({
             "first_name": "Manashree",
             "last_name": "Patel",
-            "username": "manashree@gmail.com",
+            "username": "man@gmail.com",
             "password": "mannu",
         });
 
-        const credentials = 'manashree@gmail.com:mannu';
+        const credentials = 'man@gmail.com:mannu';
         const base64Credentials = Buffer.from(credentials).toString('base64');
 
         const userResponse = await request.get(`/v1/user/self`).set('Authorization', `Basic ${base64Credentials}`);
-
+        console.log('userResponse', userResponse);
         expect(userResponse.status).to.equal(200);
+       
     });
 
 
-    it('Test 2: Update the account and validate it was updated', async () => {
-        const credentials = 'manashree@gmail.com:mannu';
+    it('Update the account information', async () => {
+    
+        const credentials = 'man@gmail.com:mannu';
         const base64Credentials = Buffer.from(credentials).toString('base64');
 
         await request.put('/v1/user/self').set('Authorization', `Basic ${base64Credentials}`).send({
@@ -39,6 +40,7 @@ describe('Integration Tests for user api', () => {
         const userResponse = await request.get('/v1/user/self').set('Authorization', `Basic ${base64Credentials}`);
 
         expect(userResponse.status).to.equal(200);
+       
     });
 
 })
