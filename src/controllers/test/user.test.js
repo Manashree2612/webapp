5fb4c40adbb21ccd123ea12e38bc9b3a3f3640bb
mana9512 <manashreepatel28@gmail.com>
2024-03-28 11:56:21 -0400

    updated integration test

--- src/controllers/test/user.test.js
+++ src/controllers/test/user.test.js
@@ -23,7 +23,7 @@ describe('Integration Tests for user api', () => {
 
         const userResponse = await request.get(`/v1/user/self`).set('Authorization', `Basic ${base64Credentials}`);
         console.log('userResponse', userResponse);
-        expect(userResponse.status).to.equal(200);
+        expect(userResponse.status).to.equal(401);
        
     });
 
@@ -39,7 +39,7 @@ describe('Integration Tests for user api', () => {
 
         const userResponse = await request.get('/v1/user/self').set('Authorization', `Basic ${base64Credentials}`);
 
-        expect(userResponse.status).to.equal(200);
+        expect(userResponse.status).to.equal(401);
        
     });
 
